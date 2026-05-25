86f9d1346f738336f624be8489c600023cab6589
reactor02 <gandbguy@gmail.com>
2026-05-26 00:00:25 +0900

    lot 목록 품목분류 필터·연계드롭다운 추가, 장비 제외, 상태값 한글화, 페이지 사이즈 5개

--- src/main/java/kr/or/smartfarm/lot/LotDAO.java
+++ src/main/java/kr/or/smartfarm/lot/LotDAO.java
@@ -2,7 +2,10 @@ package kr.or.smartfarm.lot;
 
 import java.util.List;
 
+import kr.or.smartfarm.prod.SelectOptionDTO;
+
 public interface LotDAO {
-    List<LotDTO> getList(LotPageDTO page);
-    LotDTO getSelectOne(String lot_code);
+    List<LotDTO>          getList(LotPageDTO page);
+    LotDTO                getSelectOne(String lot_code);
+    List<SelectOptionDTO> getItemOptions();
 }
--- src/main/java/kr/or/smartfarm/lot/LotPageDTO.java
+++ src/main/java/kr/or/smartfarm/lot/LotPageDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class LotPageDTO {
     private int page      = 1;
-    private int size      = 10;
+    private int size      = 5;
     private int blockSize = 10;
 
     private int totalCount;
@@ -19,6 +19,7 @@ public class LotPageDTO {
     private String startDate  = "";
     private String endDate    = "";
     private String lot_status = "";
+    private String item_type  = "";
     private int    item_num;
     private String keyword    = "";
 }
