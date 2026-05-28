f7b3d390c4e697d7c1adc4e77af57dcae63dee0d
songjonghan96@gmail.com <>
2026-05-28 17:27:35 +0900

    공정 관리 페이지 수정

--- src/main/java/kr/or/smartfarm/process/ProcessController.java
+++ src/main/java/kr/or/smartfarm/process/ProcessController.java
@@ -1,5 +1,6 @@
 package kr.or.smartfarm.process;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageInfo;
 
@@ -39,4 +41,55 @@ public class ProcessController {
 		model.addAttribute("resultList", result);
 		return "content/processDetail.tiles";
 	}
+	
+	//검색 버튼 눌러서 검색
+	//검색 버튼으로 검색했을 때
+		@RequestMapping("/searchProcess") 
+		@ResponseBody
+		public Map searchStocks(
+		        @RequestParam(value = "page", defaultValue = "1") int page,
+		        @RequestParam(value = "type") String type,
+		        @RequestParam(value = "keyword") String keyword,
+		        @RequestParam(value = "process_status") String process_status
+		        ) {
+			System.out.println("process_status===" + process_status);
+		    Map result = new HashMap();
+		    
+		    try {
+		        Map searchMap = new HashMap();
+		        searchMap.put("page", page);
+		        searchMap.put("type", type);
+		        searchMap.put("keyword", keyword );
+		        searchMap.put("process_status", process_status);
+	System.out.println(searchMap);
+		        
+		        List searchResult = Service.searchProcess(searchMap);
+		        result.put("searchResult", searchResult);
+
+		        result.put("status", "good");
+		        if(searchResult != null) {
+		        	PageInfo<Map<String, Object>> pageInfo = new PageInfo<Map<String, Object>>(searchResult);
+		        	result.put("pageInfo", pageInfo);
+		        }else {
+		        	PageInfo pageInfo = new PageInfo();
+		        	result.put("pageInfo", pageInfo);
+		        }
+		        
+		    } catch(Exception e) {
+		        e.printStackTrace();
+		        result.put("status", "error");
+		    }
+		    
+		    return result;
+		}
+		
+		//모달에서 검색
+		@RequestMapping("/processModal")
+		@ResponseBody
+		public Map modal(@RequestParam(value = "search")String keyword) {
+			Map result = null;
+			System.out.println(keyword);
+			result = Service.modalSearch(keyword);
+			return result;
+		}
 }
--- src/main/java/kr/or/smartfarm/process/ProcessDAOImpl.java
+++ src/main/java/kr/or/smartfarm/process/ProcessDAOImpl.java
@@ -1,6 +1,7 @@
 package kr.or.smartfarm.process;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,5 +32,25 @@ public class ProcessDAOImpl implements ProcessDAO{
 			result = sqlSession.selectList("kr.or.process.PdetailSelect", itemNum);
 			return result;
 			
+		}
+		
+		//목록페이지 검색
+		@Override
+		public List searchProcess2(Map map) {
+			List result = null;
+			int pageNum = (Integer)map.get("page");
+			PageHelper.startPage(pageNum, 5);
+			result = sqlSession.selectList("kr.or.process.searchProcess", map);
+			return result;
+		}
+		
+		//모달 셀렉트 
+		@Override
+		public List modalSearch2(String str) {
+
+			System.out.println("!" + str);
+			List result = sqlSession.selectList("kr.or.process.processModalSearch", str);
+			System.out.println(result);
+			return result;
 		}
 }
